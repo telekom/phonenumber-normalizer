01df3a291ac1982d255a8f2308f0dd631762e6bc
Emil Thies <Github@anrufliste.com>
2024-02-04 15:48:56 +0100

    Extended the values of PhoneNumberValidationResult enumeration to cover policies found when creating testcases for validation of numbers against the German number plan

--- src/main/java/de/telekom/phonenumbernormalizer/numberplans/PhoneNumberValidationResult.java
+++ src/main/java/de/telekom/phonenumbernormalizer/numberplans/PhoneNumberValidationResult.java
@@ -10,40 +10,69 @@ import com.google.i18n.phonenumbers.PhoneNumberUtil.ValidationResult;
  * It differentiate two possible positive and five possible negative results. The value {@link ValidationResult#INVALID_LENGTH} for any negative case, which is not explicitly covered by any of the other four values.
  * While most of the values for negative cases are focused on the number length, the value {@link ValidationResult#INVALID_COUNTRY_CODE} explicitly focus on a specific number part - the Country Code.
  * </p><p>
- * Validation of other parts of the number are not covered by an own value. So this enum wrapper is introducing {@link PhoneNumberValidationResult#INVALID_NATIONAL_ACCESS_CODE} for number plans with a national access code.
+ *  This wrapper introduces explicit INVALID_ reason codes for currently undifferentiated number parts IDP, NAC and NDC. Lastly there is also {@link PhoneNumberValidationResult#INVALID_DRAMA_NUMBER} reason code for the whole number if it is reserved as a fictional number in arts.
+ *  </p><p>
+ *  Furthermore it introduces additional IS_POSSIBLE_ values, to hint for more specific calling restrictions of the validated number besides {@link PhoneNumberValidationResult#IS_POSSIBLE_LOCAL_ONLY}.
  * </p>
  * @see ValidationResult
  */
 
-
-// TODO: DRAMA numbers: https://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Telekommunikation/Unternehmen_Institutionen/Nummerierung/Rufnummern/Mittlg148_2021.pdf?__blob=publicationFile&v=1
-
 public enum PhoneNumberValidationResult {
 
-    /** The number length matches that of valid numbers for this region. */
+    /** The number length matches that of valid numbers for this region or used NDC without any identified calling restrictions. */
     IS_POSSIBLE(ValidationResult.IS_POSSIBLE),
     /**
-     * The number length matches that of local numbers for this region only (i.e. numbers that may
-     * be able to be dialled within an area, but do not have all the information to be dialled from
+     * The number length/pattern matches that of local numbers for this region only (i.e. numbers that may
+     * be able to be dialled within an area, but does not have all the information e.g. NDC or is not allowed to be dialled from
      * anywhere inside or outside the country).
      */
     IS_POSSIBLE_LOCAL_ONLY(ValidationResult.IS_POSSIBLE_LOCAL_ONLY),
-    /** The number has an invalid country calling code. */
+    /**
+     * The number length/pattern matches that of national numbers for this region only (i.e. numbers that may
+     * be able to be dialled within an area or the region itself, but is not allowed to be dialled from
+     * anywhere outside the country using CC).
+     */
+    IS_POSSIBLE_NATIONAL_ONLY(ValidationResult.IS_POSSIBLE_LOCAL_ONLY),
+    /**
+     * The number length/pattern matches that of international number for this region only (i.e. number is not allowed to be call from within the region,
+     * even if CC of the regions is used).
+     */
+    IS_POSSIBLE_INTERNATIONAL_ONLY(ValidationResult.IS_POSSIBLE),
+    /**
+     * The number length/pattern matches that of national VPN number for this region only (i.e. number is only allowed to be call from other numbers of same VPN within this region and not by users of the public telephony network).
+     */
+    IS_POSSIBLE_NATIONAL_VPN_ONLY(ValidationResult.IS_POSSIBLE_LOCAL_ONLY),
+    /**
+     * The number length/pattern matches that of VPN number for this region only (i.e. number is only allowed to be call from other numbers of same VPN even internationally but not by users of the public telephony network).
+     */
+    IS_POSSIBLE_VPN_ONLY(ValidationResult.IS_POSSIBLE),
+    /**
+     * The number length/pattern matches that of operator traffic control numbers for this region only (i.e. number is only allowed to be call between operators for call routing and not directly by users of the public telephony network).
+     */
+    IS_POSSIBLE_OPERATOR_ONLY(ValidationResult.IS_POSSIBLE),
+    /** The number has an invalid international dialing prefix (aka IDP) for this region. */
+    INVALID_INTERNATIONAL_DIALING_PREFIX(ValidationResult.INVALID_LENGTH),
+    /** The number has an invalid national destination code (aka NDC) for this region. */
     INVALID_COUNTRY_CODE(ValidationResult.INVALID_COUNTRY_CODE),
-    /** The number has an invalid national access code. */
+    /** The number has an invalid national access code (aka NAC). */
     INVALID_NATIONAL_ACCESS_CODE(ValidationResult.INVALID_LENGTH),
-    /** The number is shorter than all valid numbers for this region. */
+    /** The number has an invalid country calling code (aka CC). */
+    INVALID_NATIONAL_DESTINATION_CODE(ValidationResult.INVALID_LENGTH),
+    /** The number is shorter than all valid numbers for this region or used NDC. */
     TOO_SHORT(ValidationResult.TOO_SHORT),
     /**
-     * The number is longer than the shortest valid numbers for this region, shorter than the
-     * longest valid numbers for this region, and does not itself have a number length that matches
+     * The number is longer than the shortest valid numbers for this region or used NDC, shorter than the
+     * longest valid numbers for this region or used NDC, and does not itself have a number length that matches
      * valid numbers for this region. This can also be returned in the case where
      * isPossibleNumberForTypeWithReason was called, and there are no numbers of this type at all
-     * for this region.
+     * for this region or used NDC while none of the other INVALID values would match.
      */
     INVALID_LENGTH(ValidationResult.INVALID_LENGTH),
-    /** The number is longer than all valid numbers for this region. */
-    TOO_LONG(ValidationResult.TOO_LONG);
+    /** The number is longer than all valid numbers for this region, or for the used NDC. */
+    TOO_LONG(ValidationResult.TOO_LONG),
+    /** The number is matching a drama number range, which simulates valid number for this region only used in movies or other fictional story telling. */
+    INVALID_DRAMA_NUMBER(ValidationResult.INVALID_LENGTH);
+
     /**
      * storing the corresponding enum value of {@link ValidationResult}
      */
