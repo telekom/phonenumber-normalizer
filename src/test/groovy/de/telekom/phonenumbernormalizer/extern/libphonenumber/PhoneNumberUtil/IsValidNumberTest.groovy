e9a785c4411e6d0a6886a88123a4dc7644b1ea67
Emil Thies <Github@anrufliste.com>
2024-04-28 08:52:28 +0200

    Cloning testset from IsPossibleNumberWithReasonTest to IsValidNumberTest: German traffic routing 01988 for international free calls

--- src/test/groovy/de/telekom/phonenumbernormalizer/extern/libphonenumber/PhoneNumberUtil/IsValidNumberTest.groovy
+++ src/test/groovy/de/telekom/phonenumbernormalizer/extern/libphonenumber/PhoneNumberUtil/IsValidNumberTest.groovy
@@ -2775,6 +2775,64 @@ class IsValidNumberTest extends Specification {
         "+491987"   | true     | "FR" | [false, false, false, true, false, false, false, false, false, true, false, false, false]
     }
 
+    def "check if original lib fixed isValid for German traffic routing 01988 for international free calls"(String reserve, operator,regionCode, boolean[] expectingFails) {
+        given:
+        String[] numbersToTest = [reserve + "",
+                                  reserve + "0",
+                                  reserve + "00",
+                                  reserve + "000",
+                                  reserve + "0000",
+                                  reserve + "00000",
+                                  reserve + "000000",
+                                  reserve + "9",
+                                  reserve + "99",
+                                  reserve + "999",
+                                  reserve + "9999",
+                                  reserve + "99999",
+                                  reserve + "999999"]
+
+        Boolean[] expectedResults
+        if ((operator)) {
+            expectedResults = [false, false,
+                               true, // not callable public, but for operators
+                               false, false, false, false,
+                               false,
+                               true, // not callable public, but for operators
+                               false, false, false, false]
+        } else {
+            expectedResults = [false, false,
+                               false, // not callable public, but for operators
+                               false, false, false, false,
+                               false,
+                               false, // not callable public, but for operators
+                               false, false, false, false]
+        }
+
+        when:
+        Boolean[] results = []
+        for (number in numbersToTest) {
+            def phoneNumber = phoneUtil.parse(number, regionCode)
+            results += phoneUtil.isPossibleNumberWithReason(phoneNumber)
+        }
+
+        then:
+        for (int i = 0; i < results.length; i++) {
+            this.logResult(results[i], expectedResults[i], expectingFails[i], numbersToTest[i], regionCode)
+        }
+
+        where:
+        reserve     | operator | regionCode | expectingFails
+        //  0198 is trafic control: https://www.bundesnetzagentur.de/DE/Fachthemen/Telekommunikation/Nummerierung/Verkehrslenkungsnummern/start.html
+        //  Number Plan https://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Telekommunikation/Unternehmen_Institutionen/Nummerierung/Rufnummern/Verkehrslenkungsnr/NummernplanVerkehrslenkungsnrn.pdf?__blob=publicationFile&v=1
+        //  01988 is used for EU public service call routing from operators and are not callable by normal public telephony network users (TODO: verfiy it is callable by international operators, which is assumed, because +49 is usable (unlike at 01981)
+        //  01988-xx TODO: verify called number information is transfered outside the number (no digits after xx)
+        //  for traditional libphone it makes no difference if number is used by public user or operator, so one of it will always fail until it could distinguish it
+        "01988"     | false    | "DE" | [true, true, true, true, true, true, true, true, true, true, true, true, true]
+        "01988"     | true     | "DE" | [true, true, false, true, true, true, true, true, false, true, true, true, true]
+        "+491988"   | false    | "FR" | [true, true, true, true, true, true, true, true, true, true, true, true, true]
+        "+491988"   | true     | "FR" | [true, true, false, true, true, true, true, true, false, true, true, true, true]
+    }
+
 
     def "check if original lib fixed isValidNumber for invalid German NDC"(String number, regionCode, expectedResult, expectingFail) {
         given:
