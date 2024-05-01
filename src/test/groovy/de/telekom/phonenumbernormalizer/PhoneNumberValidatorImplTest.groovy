751b2262866a6ac2493132e353d1ae7f2ea4bef7
Emil Thies <Github@anrufliste.com>
2024-05-01 14:57:11 +0200

    Prepare new class PhoneNumberValidator - currently just reusing PhoneLibWrapper to access PhoneLib logic. First testcase set taken from PhoneNumberNormalizer as starting point. Define expected behaviour and outcomment all testcases which currently do not work.
    Those can be used to test logic addition to capture more detailed behaviour than phone lib. After standard cases work, next phase will be to include special testcases from PhoneNumberUtil test folder.

--- src/test/groovy/de/telekom/phonenumbernormalizer/PhoneNumberValidatorImplTest.groovy
+++ src/test/groovy/de/telekom/phonenumbernormalizer/PhoneNumberValidatorImplTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2024 Deutsche Telekom AG (opensource@telekom.de)
+ * Copyright © 2023 Deutsche Telekom AG (opensource@telekom.de)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
