2b4953629d969534a24b434c5723b558ff560436
Emil Thies <Github@anrufliste.com>
2024-05-20 21:45:22 +0200

    Extend Number Plan to provide multi dimensional short code information.

--- src/main/java/de/telekom/phonenumbernormalizer/numberplans/constants/DeFixedLineNumberPlan.java
+++ src/main/java/de/telekom/phonenumbernormalizer/numberplans/constants/DeFixedLineNumberPlan.java
@@ -16,9 +16,47 @@
 package de.telekom.phonenumbernormalizer.numberplans.constants;
 
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import de.telekom.phonenumbernormalizer.numberplans.NumberPlan;
+import lombok.RequiredArgsConstructor;
+
+
+class ShortNumberDetails {
+    int length;
+
+    boolean usableWithIDPandCCfromOutside;
+
+    boolean usableWithIDPandCCandNDCfromOutside ;
+
+    boolean usableWithIDPandCCfromInside;
+
+    boolean usableWithIDPandCCandNDCfromInside;
+
+    boolean usableWithNAC;
+    boolean usableWithNACandNDC;
+
+    boolean usableDirectly;
+
+    public ShortNumberDetails(int length, boolean usableWithIDPandCCfromOutside,
+                                          boolean usableWithIDPandCCandNDCfromOutside,
+                                          boolean usableWithIDPandCCfromInside,
+                                          boolean usableWithIDPandCCandNDCfromInside,
+                                          boolean usableWithNAC,
+                                          boolean usableWithNACandNDC,
+                                          boolean usableDirectly) {
+        this.length = length;
+        this.usableWithIDPandCCfromOutside = usableWithIDPandCCfromOutside;
+        this.usableWithIDPandCCandNDCfromOutside = usableWithIDPandCCandNDCfromOutside;
+        this.usableWithIDPandCCfromInside = usableWithIDPandCCfromInside;
+        this.usableWithIDPandCCandNDCfromInside = usableWithIDPandCCandNDCfromInside;
+        this.usableWithNAC = usableWithNAC;
+        this.usableWithNACandNDC = usableWithNACandNDC;
+        this.usableDirectly = usableDirectly;
+    }
+}
 
 /**
  *  Definition see Chapter 8.1 in <a href="https://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Telekommunikation/Unternehmen_Institutionen/Nummerierung/Nummerierungskonzept/Nummerierungskonzept2011pdf.pdf?__blob=publicationFile">BNetzA German Number Plan</a>
@@ -32,17 +70,56 @@ public class DeFixedLineNumberPlan extends NumberPlan {
     private static final String COUNTRY_CODE = "49";
 
     /**
-     * Constant for German short numbers in fixed-line
+     * Constants for German short numbers in fixed-line
      */
-    private static final Map<String, Integer> SHORT_NUMBER_CODES = Map.of(
-            "110", 3,
-            "112", 3,
-            "115", 3,
-            "116", 6,
-            "1180", 6,
-            "118", 5  // This covers  1181 - 1189 since 1180 is longer prefix and has its own value.
+    private static final Map<String, ShortNumberDetails> SHORT_NUMBER_CODES_DETAILS = Map.of(
+            "110", new ShortNumberDetails(3, false, false, false, false, false, false, true),
+            "112", new ShortNumberDetails(3, false, false, false, false, false, false, true),
+            "115", new ShortNumberDetails(3, false, false, false, false, false, false, true),
+            "116", new ShortNumberDetails(6, false, false, false, false, false, false, true),
+            "1180", new ShortNumberDetails(6, false, false, false, false, false, false, true),
+            "118", new ShortNumberDetails(5, false, false, false, false, false, false, true)  // This covers  1181 - 1189 since 1180 is longer prefix and has its own value.
     );
 
+    /**
+     * Constant for German short numbers in fixed-line as extracted from the details above
+     */
+    private static final Map<String, Integer> SHORT_NUMBER_CODES = SHORT_NUMBER_CODES_DETAILS.entrySet().stream()
+        .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().length));
+
+    @Override
+    public boolean isUsableWithIDPandCCfromOutside(String number) {
+        return SHORT_NUMBER_CODES_DETAILS.get(startingWithShortNumberKey(number)).usableWithIDPandCCfromOutside;
+    }
+
+    @Override
+    public boolean isUsableWithIDPandCCandNDCfromOutside(String number) {
+        return SHORT_NUMBER_CODES_DETAILS.get(startingWithShortNumberKey(number)).usableWithIDPandCCandNDCfromOutside;
+    }
+
+    @Override
+    public boolean isUsableWithIDPandCCfromInside(String number) {
+        return SHORT_NUMBER_CODES_DETAILS.get(startingWithShortNumberKey(number)).usableWithIDPandCCfromInside;
+    }
+
+    @Override
+    public boolean isUsableWithIDPandCCandNDCfromInside(String number) {
+        return SHORT_NUMBER_CODES_DETAILS.get(startingWithShortNumberKey(number)).usableWithIDPandCCandNDCfromInside;
+    }
+
+    @Override
+    public boolean isUsableWithNAC(String number) {
+        return SHORT_NUMBER_CODES_DETAILS.get(startingWithShortNumberKey(number)).usableWithNAC;
+    }
+    @Override
+    public boolean isUsableWithNACandNDC(String number) {
+        return SHORT_NUMBER_CODES_DETAILS.get(startingWithShortNumberKey(number)).usableWithNACandNDC;
+    }
+
+    @Override
+    public boolean isUsableDirectly(String number) {
+        return SHORT_NUMBER_CODES_DETAILS.get(startingWithShortNumberKey(number)).usableDirectly;
+    }
 
     @Override
     protected Map<String, Integer> getShortNumberCodes() {
@@ -51,6 +128,25 @@ public class DeFixedLineNumberPlan extends NumberPlan {
 
     public static String getCountryCode() {
         return COUNTRY_CODE;
+    }
+
+    @Override
+    public String getNationalDestinationCodeFromNationalSignificantNumber(String nsn) {
+        if ((nsn == null) || (nsn.length()<1)) {
+            return "";
+        }
+
+        if ("1".equals(nsn.substring(0,1))) {
+          // Non-Geographic Area Codes
+            if (nsn.length()<2) {
+                return "";
+            }
+
+
+
+        }
+        // Geographic Area Codes
+        return GermanAreaCodeExtractor.fromNumber(nsn);
     }
 
 }
